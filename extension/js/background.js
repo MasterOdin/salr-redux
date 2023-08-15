f6d06994dd118ac5c2f509d4d5bb599b0a1fba84
John <loneboco@gmail.com>
2023-08-14 18:21:01 -0700

    Auto reconnect runtime port if disconnected (#104)

--- extension/js/background.js
+++ extension/js/background.js
@@ -35,18 +35,36 @@ var salr_client = false;
  * within the extension.
  *
  */
-var port = chrome.runtime.connect();
+var port = createPort();
 
-port.onMessage.addListener(function(data) {
-    if (!salr_client) {
-        salr_client = new SALR(data, chrome.extension.getURL("images/"));
-    }
+// Set up the listener for when we request our settings.
+port.onMessage.addListener(function init(data) {
+  salr_client = new SALR(data, chrome.extension.getURL("images/"));
+  port.onMessage.removeListener(init);
 });
 
-// Request the settings from the extension
+// Request the settings from the extension.
 port.postMessage({'message': 'GetPageSettings'});
 
-// Gateway method for components to post to the extension
+// Create a port to communicate through.
+function createPort() {
+    let p = chrome.runtime.connect();
+
+    // If we disconnect, clear our port variable so we know the reconnect the port when we next send a message.
+    // Chromium browsers keep the port open for a while, but Firefox can very quickly close the port.
+    p.onDisconnect.addListener(() => {
+        port = null;
+        p = null;
+    });
+
+    return p;
+}
+
+// Gateway method for components to post to the extension.
 function postMessage(message_object) {
+    if (port === null) {
+        port = createPort();
+    }
+
     port.postMessage(message_object);
 }
